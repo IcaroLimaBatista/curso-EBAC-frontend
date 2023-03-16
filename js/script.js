d519ae25d2b4b3f0f58870b6657dda3378f9c184
Ícaro Lima Batista <icarolimabatista@gmail.com>
2023-03-16 15:24:00 -0300

    Conserto da atividade

--- js/script.js
+++ js/script.js
@@ -6,12 +6,9 @@ function comparacao(){
     var maior = document.querySelector("h3");
     if ((n1.value && n2.value !== "") && (n1.value && n2.value !== null) && (n1.value && n2.value !== undefined)&& (n1.value <= 100 && n2.value <= 100 )){
         if (n1.value > n2.value){
-            maior.innerHTML = `O maior número é ${n1.value}`;
-        }else if(n1.value < n2.value){
-            maior.innerHTML = `O maior número é ${n2.value}`;
+            maior.innerHTML = `Válido`;
         }else{
-            maior.innerHTML = `Deu empate, o número é <br>${n1.value}`
-            
+            maior.innerHTML = `Inválido`
         }
     }
 }
