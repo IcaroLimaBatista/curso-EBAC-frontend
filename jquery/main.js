d1118af36dbbd4f6ddb0eef9d87ace6a81a17add
Ícaro Lima Batista <icarolimabatista@gmail.com>
2023-03-21 09:05:51 -0300

    Exercicio jQuery

---
+++ jquery/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    
+    let form = $("form");
+
+    form.submit(function(e){
+        e.preventDefault();    
+        let inputForm= $("#input-form");
+        if(inputForm.val().length <= 100){
+            
+            // ADICIONAR NOVO ITEM SE CARACTERES FOR ABAIXO OU IGUAL 100
+            let novoItem = $(`<li style = "display:none;">
+            <h2>${inputForm.val()}</h2>
+            </li>`).appendTo("ul");
+            novoItem.fadeIn(1000);
+
+            // RISCO NO TEXTO AO CLICAR NA TAREFA
+            $("li").click(function(){
+                $("li").css("text-decoration", "line-through");
+           })  
+        }
+
+    })
+
+    
+})
