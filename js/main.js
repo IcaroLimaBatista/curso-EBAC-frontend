eb03f2d332c000f20aeef999eaf8aa423adaf4ac
Ícaro Lima Batista <icarolimabatista@gmail.com>
2023-03-27 15:53:52 -0300

    Version with erro

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
-    $("#telefone").mask("(00) 00000-0000")
+    $("#telefone").mask("(00) 00000-0000");
+    $("#cpf").mask("000.000.0000-00");
+    $("#cep").mask("00000-000");
 
     $("form").validate({
         rules: {
@@ -24,7 +26,16 @@ $(document).ready(function(){
                 required: true
             },
 
+        },
+        messages: {
+            nome: "Por favor insira seu nome",
+            email:"Por favor insira um e-mail válido",
+            telefone: "Por favor insira um telefone válido",
+            cpf: "Por favor insira um CPF válido",
+            endereco: "Por favor insira um endereco válido",
+            cep: "Por favor insira um CEP válido"
         }
-    })
+    });
 
 })
+
